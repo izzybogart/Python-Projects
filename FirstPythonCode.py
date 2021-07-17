ef05842aae4631bf8f5ddf5d5511258c7ddb8b80
izzybogart <60237226+izzybogart@users.noreply.github.com>
2021-07-17 11:35:46 -0600

    Update FirstPythonCode.py

--- FirstPythonCode.py
+++ FirstPythonCode.py
@@ -1,4 +1,7 @@
-for i in range(20):
+
+#Runs through a range of 0 to 20, checks if the numbers are divisible by three and if so prints them,
+#also prints "Bingo!" after every number that is also divisible by 5, and finally prints "---" for 
+#numbers that are not divisible by 3.for i in range(20):
     if i%3==0:
         print(i)
         if i%5 == 0:
